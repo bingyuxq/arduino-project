46e1232715f9b89717ee132745b644ac68a977c5
bingyuxq <xyz50270@gmail.com>
2015-10-20 02:38:09 +0800

    完成了通过web server向ESP8266提交命令的雏形代码
    Signed-off-by: bingyuxq <xyz50270@gmail.com>

--- 红外遥控器/senderESP8266/senderESP8266.ino
+++ 红外遥控器/senderESP8266/senderESP8266.ino
@@ -4,49 +4,152 @@
  * Version 0.1 June, 2015
  * Based on Ken Shirriff's IrsendDemo Version 0.1 July, 2009, Copyright 2009 Ken Shirriff, http://arcfn.com
  */
-
+#include <ESP8266WiFi.h>
 #include <IRremoteESP8266.h>
 
-IRsend irsend(2); //an IR led is connected to GPIO pin 2
+const char* ssid = "bingyu-AP";
+const char* password = "bingyuxq";
+// Create an instance of the server
+// specify the port to listen on as an argument
+WiFiServer server(80);
 
-String Command = "";
+IRsend irsend(13); //an IR led is connected to GPIO pin 13
 
 void setup()
 {
   irsend.begin();
-  Serial.begin(9600);
+  Serial.begin(115200);
+
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+  // Print the IP address
+  Serial.println(WiFi.localIP());
 }
 
-bool RemoteSender()
+//ALib Begin//
+char** cSplit(char *string, char spliter, int* arrayCount)
 {
-  int j;
-  int RawLength = Serial.parseInt();
-  unsigned int RawData[RawLength];
-  for (j = 0; j < RawLength; j++)
-  {
-    RawData[j] = Serial.parseInt();
+  //分割后字符串数量
+  int num = 0;
+  //计算数量
+  int i = 0;
+  int size = strlen(string);
+  for (; i < size; i++) {
+    if ((string[i] != spliter) && (string[i + 1] == spliter || string[i + 1] == '\0'))
+      num++;
   }
+  *arrayCount = num;
+  //分配空间
+  char **cStrings = (char**)malloc(sizeof(char*) * (num + 1));
+  int j = 0;
+  for (; j <= num; j++) {
+    //初始化指针数组
+    cStrings[j] = 0;
+  }
+  //保存分割后字符串
+  char *buffer = (char*)malloc(sizeof(char) * (size + 1));
+  int l = 0;
+  for (; l < size + 1; l++) {
+    buffer[l] = 0;
+  }
+  int counter = 0;
+  int k = 0;
+  for (; k < size; k++) {
+    if (string[k] != spliter) {
+      int i = 0;
+      int j = k;
+      while (string[j] != spliter && string[j] != '\0') buffer[i++] = string[j++];
+      buffer[i] = '\0';
+      cStrings[counter] = (char*)malloc(sizeof(char) * i);
+      strcpy(cStrings[counter++], buffer);
+      k = k + i;
+    }
+  }
+  free(buffer);
+  return cStrings;
+}
+/**
+*调用此方法释放cSplit分配的内存空间
+*/
+void freeStringsMemory(char **strings) {
+  int k = 0;
+  for (; ; k++) {
+    if (strings[k] != 0) free(strings[k]);
+    else break;
+  }
+  free(strings);
+}
+//ALib End//
+
+bool RemoteSender(char* string)
+{
+  char** stringarr;
+  int arrayCount = 0;
+  stringarr = cSplit(string, ',', &arrayCount);
+  
+  int RawLength = atoi(stringarr[0]);
+  unsigned int RawData[RawLength];
+  for (int i = 1; i < arrayCount; i++) {
+    RawData[i-1] = atoi(stringarr[i]);
+  }
+
   irsend.sendRaw(RawData, RawLength, 38);
   delay(100);
   Serial.println("done");
+
 }
 
 void loop() {
-  if (Serial.available() > 0)
-  {
-    //读取前10个字符，这部分作为命令识别码
-    int i;
-    for (i = 0; i < 10; i++)
-    {
-      Command += (char)Serial.read();
-      delay(2);
-    }
-    Serial.println(Command);
-    //识别命令,RemoteSend,68,8850,4500,500,600,500,650,450,650,450,1750,500,600,500,650,450,600,500,650,500,600,500,1750,450,1750,450,650,500,600,500,1750,450,1750,450,1750,500,600,500,600,500,650,450,1800,450,1750,450,650,450,1750,500,600,500,1750,450,1750,450,1750,500,650,450,650,450,1750,450,650,500,1750,450
-    if (Command == "RemoteSend")
-    {
-      RemoteSender();
-    }
-    Command = "";
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
   }
+
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while (!client.available()) {
+    delay(1);
+  }
+
+  // Read the first line of the request
+  String req = client.readStringUntil(',');
+  if (req.indexOf("RemoteSend") != -1)
+  {
+    Serial.println("Get command RemoteSend");
+    req = client.readStringUntil('\r');
+    RemoteSender((char*)req.c_str());
+  }
+  client.flush();
+
+
+  client.flush();
+
+  // Prepare the response
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\ndone";
+  s += "</html>\n";
+
+  // Send the response to the client
+  client.print(s);
+  delay(1);
+  Serial.println("Client disonnected");
+
+  // The client will actually be disconnected
+  // when the function returns and 'client' object is detroyed
 }
