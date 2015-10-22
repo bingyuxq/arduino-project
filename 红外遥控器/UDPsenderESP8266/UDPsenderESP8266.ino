525c74cc26313f21bb33bce2cd48db5ffcac0cf9
bingyuxq <xyz50270@gmail.com>
2015-10-23 02:40:43 +0800

    解码部分完成，但是发送部分有个内存泄露的问题，等待修复
    Signed-off-by: bingyuxq <xyz50270@gmail.com>

--- 红外遥控器/UDPsenderESP8266/UDPsenderESP8266.ino
+++ 红外遥控器/UDPsenderESP8266/UDPsenderESP8266.ino
@@ -6,6 +6,7 @@ const char* host = "esp8266-ota";
 const char* ssid = "bingyu-AP";
 const char* pass = "bingyuxq";
 IRsend irsend(13); //an IR led is connected to GPIO pin 13
+IRrecv irrecv(5); //an IR receiver is connected to GPIO pin 5
 unsigned int localPort = 8888;      // local port to listen on
 char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
 char  ReplyBuffer[] = "acknowledged";       // a string to send back
@@ -55,35 +56,87 @@ void loop() {
     Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
     Serial.println("Contents:");
     Serial.println(packetBuffer);
-    //if(packetBuffer.indexOf("RemoteSender")!=-1){
-    if(strchr(packetBuffer,'R')){
-    RemoteSender(packetBuffer);
-    }
 
-    // send a reply, to the IP address and port that sent us the packet we received
-    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-    Udp.write(ReplyBuffer);
-    Udp.endPacket();
+    if (strchr(packetBuffer, 'R')) {
+      RemoteSender(packetBuffer);
+    }
+    if (strchr(packetBuffer, 'L')) {
+      LearnCommand(packetBuffer);
+    }
   }
   delay(10);
 }
 
 void RemoteSender(char* string)
 {
-  char *dest[128];
+  char *dest[500];
   int num = 0;
   split(string, ",", dest, &num);
-  Serial.println("sending");
+  Serial.println("alive1");
   unsigned int RawData[num];
   for (int i = 1; i <= num; i++) {
+  Serial.println(i);
     //unsigned int RawData[68] = {8900,4450,500,600,550,550,550,600,500,1700,500,600,550,600,500,550,550,550,550,600,500,1750,500,1700,500,550,550,600,500,1700,550,1650,550,1700,500,1750,500,1700,500,1750,450,650,500,550,550,600,500,1700,500,600,500,600,550,550,550,550,550,1700,500,1750,500,1700,500,600,500,1700,550};
     RawData[i - 1] = atoi(dest[i]);
   }
+  Serial.println("alive2");
   irsend.sendRaw(RawData, num-1, 38);//38kHz
   delay(100);
-  Serial.println("send complete");
+  
+  Serial.println("alive3");
+  Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+  Udp.write("Command Sent");
+  Udp.endPacket();
+  Serial.println("alive4");
 }
 
+void LearnCommand(char* string)
+{
+  irrecv.enableIRIn();  // Start the receiver
+  //char *dest[128];
+  // send a reply, to the IP address and port that sent us the packet we received
+  Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+  Udp.write("Beat Me");
+  Udp.endPacket();
+  delay(500);
+  int BeginMsec = millis() + 30000;
+  do
+  {
+    decode_results  results;        // Somewhere to store the results
+
+    if (irrecv.decode(&results)) {  // Grab an IR code
+      int Length = results.rawlen - 1;
+      unsigned int RawData[Length];
+
+      Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+      for (int i = 0;  i < results.rawlen;  i++) {
+        unsigned int Data = results.rawbuf[i] * USECPERTICK;
+        /*if (!(i & 1)) {Serial.print("-");Serial.print(results->rawbuf[i] * USECPERTICK, DEC);} //odd
+        else{Serial.print("+");Serial.print(results->rawbuf[i] * USECPERTICK, DEC);} //even*/
+        char buff[6];
+        itostr(buff, Data);
+        Udp.write(buff);
+        if ( i < Length ) Udp.write(",");
+      }
+      Udp.endPacket();
+      delay(500);
+      //dumpRaw(&results);            // Output the results in RAW format
+      irrecv.disableIRIn();
+      Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+      Udp.write("Got it");
+      Udp.endPacket();
+      return;
+    }
+    delay(10);
+  } while (BeginMsec > millis());
+  irrecv.disableIRIn();
+}
+
+char* itostr(char *str, int i)
+{
+  sprintf(str, "%d", i);
+  return str;
+}
 //char[]来源，const char分隔符，char*目标缓存数组的指针(buff为char* []类型，int元素数缓存
 void split(char *src, const char *separator, char **dest, int *num)
 {
